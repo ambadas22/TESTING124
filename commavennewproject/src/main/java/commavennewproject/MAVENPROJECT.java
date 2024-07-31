408668b4f3255a8903103d2dd75b7d970ac2fbe2
admin <admin@DESKTOP-56519KS>
2024-07-31 14:14:28 +0530

    "Adding a new Maven Project"

---
+++ commavennewproject/src/main/java/commavennewproject/MAVENPROJECT.java
@@ -0,0 +1,33 @@
+package commavennewproject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MAVENPROJECT {
+	
+	
+	@Test
+	public void checkloginpage1() throws InterruptedException {
+		
+		System.out.println("dashboard test1");
+		
+//		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+//
+//		ChromeDriver driver= new ChromeDriver();
+//		driver.manage().window().maximize();
+//		driver.get("http://192.168.168.101:8091/WelcomePage.aspx");
+//
+//		driver.findElement(By.id("txtUsername")).sendKeys("ittest");
+//		driver.findElement(By.id("txtPassword")).sendKeys("654");
+//		driver.findElement(By.id("btnLogin")).click();
+//		Thread.sleep(2000);
+//
+//		Assert.assertEquals("http://192.168.168.101:8091/WelcomePage.aspx", driver.getCurrentUrl());
+//		Thread.sleep(2000);
+//		driver.quit();	
+	}
+	
+
+}
